d8c7f62fc9cc0a7a8ad74d3a088b294536599300
Micha Sende <sende@lakeside-labs.com>
2019-11-18 14:57:42 +0100

    measure and plot distances between agents

--- boid_flockers/model.py
+++ boid_flockers/model.py
@@ -1,44 +1,82 @@
 import numpy as np
+import itertools as it
 
 from mesa import Model
 from mesa.space import ContinuousSpace
 from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
 
 from .boid import Boid
 
+def minimum_distance(model):
+    '''
+    Compute the minimum distance between any agent pair.
+    '''
+    return min(model.density)
+
+def maximum_distance(model):
+    '''
+    Compute the minimum distance between any agent pair.
+    '''
+    return max(model.density)
+
+def average_distance(model):
+    '''
+    Compute the minimum distance between any agent pair.
+    '''
+    return np.mean(model.density)
+
+def collisions(model):
+    '''
+    Compute the number of agents being too close.
+    '''
+    return len([d for d in model.density if d < model.min_dist])
 
 class BoidFlockers(Model):
     '''
     A Mesa implementation of flocking agents inspired by https://doi.org/10.1109/IROS.2014.6943105.
     '''
 
-    def __init__(self, population, width, height, vision,
-                 flock_vel=      1.0,
-                 accel_time=     10.0,
-                 equi_dist=      1.0,
-                 repulse_max=    0.1,
-                 repulse_spring= 0.1,
-                 align_frict=    1.0,
-                 align_slope=    1.0,
-                 align_min=      1.0,
-                 wall_decay=     5.0,
-                 wall_frict=     1.0,
-                 wp_tolerance=   10.0):
+    def __init__(self, population, width, height, vision, min_dist, flock_vel, accel_time, equi_dist, repulse_max, repulse_spring, align_frict, align_slope, align_min, wall_decay, wall_frict, wp_tolerance):
         '''
         Create a new Flockers model.
 
         Args:
-            population: Number of agents
+            population: Number of agents.
             width, height: Size of the space.
-            vision: How far around should each agents look for its neighbors
+            vision: How far around should each agents look for its neighbors.
+            min_dist: Minimum allowed distance between agents before a collision occurs. This is only used for statistics.
         '''
+        # set parameters
         self.population = population
+        self.space = ContinuousSpace(width, height, torus=False)
         self.vision = vision
-        self.schedule = RandomActivation(self)
-        self.space = ContinuousSpace(width, height, False)
+        self.min_dist = min_dist
         self.params = dict(flock_vel=flock_vel, accel_time=accel_time, equi_dist=equi_dist, repulse_max=repulse_max, repulse_spring=repulse_spring, align_frict=align_frict, align_slope=align_slope, align_min=align_min, wall_decay=wall_decay, wall_frict=wall_frict, wp_tolerance=wp_tolerance)
+
+        # data collection for plots
+        self.datacollector = DataCollector(model_reporters={"Minimum Distance":minimum_distance, "Maximum Distance":maximum_distance, "Average Distance":average_distance, "Collisions":collisions})
+
+        # execute agents sequentially in a random order
+        self.schedule = RandomActivation(self)
+
+        # place agents
         self.make_agents()
+
+        # pairwise distances
+        self.agent_distances()
+
+        # run model
         self.running = True
+
+        # collect initial data sample
+        self.datacollector.collect(self)
+
+    def agent_distances(self):
+        '''
+        Compute the pairwise distances between all agents.
+        '''
+        self.density = [self.space.get_distance(pair[0].pos, pair[1].pos) for pair in it.combinations(self.schedule.agent_buffer(), 2)]
 
     def make_agents(self):
         '''
@@ -54,4 +92,9 @@ class BoidFlockers(Model):
             self.schedule.add(boid)
 
     def step(self):
+        '''
+        Execute one step of the simulation.
+        '''
         self.schedule.step()
+        self.agent_distances()
+        self.datacollector.collect(self)
--- boid_flockers/server.py
+++ boid_flockers/server.py
@@ -1,22 +1,21 @@
 from mesa.visualization.ModularVisualization import ModularServer
-
-from .model import BoidFlockers
-from .SimpleContinuousModule import SimpleCanvas
+from mesa.visualization.modules import ChartModule
 from mesa.visualization.UserParam import UserSettableParameter
+
+from .SimpleContinuousModule import SimpleCanvas
+from .model import BoidFlockers
 
 
 def boid_draw(agent):
     return {"Shape": "circle", "r": 1, "Filled": "true", "Color": "Red"}
     #return {"Shape": "arrowHead", "scale": 2, "heading_x": agent.velocity[0], "heading_y": agent.velocity[1], "Filled": "true", "Color": "Red"}
 
-
-boid_canvas = SimpleCanvas(boid_draw, 750, 750)
-
 model_params = {
-    "population":     5,
+    "population":     10,
     "width":          100,
     "height":         100,
     "vision":         150,
+    "min_dist":       UserSettableParameter("slider", value=1, min_value=0.1, max_value=10, step=0.1, name="minimum distance", description="Minimum allowed distance between agents before a collision occurs. This is only used for statistics."),
     "flock_vel":      UserSettableParameter("slider", value=1, min_value=0.1, max_value=10, step=0.1, name="v<sub>flock</sub>: flock velocity", description="Target velocity of the flock."),
     "accel_time":     UserSettableParameter("slider", value=10, min_value=1, max_value=100, step=1, name="&tau;: acceleration time", description="Characteristic time needed by the agent to reach the target velocity."),
     "equi_dist":      UserSettableParameter("slider", value=1, min_value=0.1, max_value=10, step=0.1, name="r<sub>0</sub>: distance", description="Equilibrium distance between agents."),
@@ -30,4 +29,9 @@ model_params = {
     "wp_tolerance":   UserSettableParameter("slider", value=10, min_value=1, max_value=100, step=1, name="waypoint tolerance", description="The distance to the current waypoint below which the next waypoint of the path is selected."),
 }
 
-server = ModularServer(BoidFlockers, [boid_canvas], "Flocking", model_params)
+boid_canvas = SimpleCanvas(boid_draw, 750, 750)
+
+boid_distance = ChartModule([{"Label":"Minimum Distance", "Color":"Red"}, {"Label":"Maximum Distance", "Color":"Green"}, {"Label":"Average Distance", "Color":"Black"}])
+boid_collisions = ChartModule([{"Label":"Collisions", "Color":"Red"}])
+
+server = ModularServer(BoidFlockers, [boid_canvas, boid_distance, boid_collisions], "Flocking", model_params)
